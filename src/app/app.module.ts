278f6c7cbe21ff6a6ffef05a65357d7f11bcfcdb
katarina-cudina <cudina.kat@gmail.com>
2024-03-07 00:47:53 +0100

    add necessary components for overview page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { ButtonComponent } from './components/button/button.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, SearchComponent, TableComponent],
   imports: [BrowserModule, AppRoutingModule, ButtonComponent, LoginFormComponent, FormInputComponent, OverviewComponent], 
   providers: [VehicleService],
   bootstrap: [AppComponent],
